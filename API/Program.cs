623318f7fc659db9763f463706a1df2af164f0d4
Jhonatan <larico1599@gmail.com>
2023-07-07 20:32:39 -0500

    Db Agregado

--- API/Program.cs
+++ API/Program.cs
@@ -23,11 +23,13 @@ builder.Services.AddScoped<IBookService, BookService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("CorsPolicy");
 
 app.UseHttpsRedirection();
 
