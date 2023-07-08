5f56e5c02e9691fc63dae9b8074070403b0088ee
Jhonatan <larico1599@gmail.com>
2023-07-07 21:07:02 -0500

    swagger modificado

--- API/Program.cs
+++ API/Program.cs
@@ -10,7 +10,14 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(setupAction =>
+{
+    setupAction.SwaggerDoc("LibraryOpenAPISpecification", new()
+    {
+        Title = "Mercado Libro API",
+        Version = "1"
+    });
+});
 
 builder.Services.AddDbContext<BookCartContext>(options =>
 {
@@ -26,7 +33,11 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(setupAction =>
+    {
+        setupAction.SwaggerEndpoint("/swagger/LibraryOpenAPISpecification/swagger.json", "MercadoLibroAPI");
+        setupAction.RoutePrefix = string.Empty;
+    });
 }
 
 app.UseCors("CorsPolicy");
